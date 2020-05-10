b002ee2351ded47c518f72fcdac8bd15c99824e2
AndrewCWheeler <achristopherwheeler@gmail.com>
2020-05-09 17:03:11 -0700

    second commit

--- app_random_word/views.py
+++ app_random_word/views.py
@@ -7,11 +7,9 @@ def index(request):
 def random_word(request):
     counter = 0
     if 'counter' not in request.session.keys():
-        print('New Session')
         request.session['counter'] = 0
     else:
         request.session['counter'] += 1
-        print(counter)
     context = {
         'counter' : request.session['counter'],
         'generate_word' : get_random_string(length=14)
@@ -19,7 +17,6 @@ def random_word(request):
     return render(request, 'random_word.html', context)
 
 def reset(request):
-    print("Resetting")
     request.session['counter'] = 0
     return redirect('/random_word')
 
